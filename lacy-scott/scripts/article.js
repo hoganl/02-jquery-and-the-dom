2070fa2f72827ec30c95ee62d96b790182581171
ScottyKnows44 <Scottcfalcon@gmail.com>
2018-03-14 13:34:19 -0700

    02 lab worked on instances

--- lacy-scott/scripts/article.js
+++ lacy-scott/scripts/article.js
@@ -11,7 +11,7 @@ function Article (rawDataObj) {
   this.title = rawDataObj.title;
   this.category = rawDataObj.category;
   this.author = rawDataObj.author;
-  this.authorURL = rawDataObj.authorURL;
+  this.authorUrl = rawDataObj.authorUrl;
   this.publishedOn = rawDataObj.publishedOn;
   this.body = rawDataObj.body;
 }
@@ -21,22 +21,24 @@ Article.prototype.toHtml = function() {
   // It copies not only matched elements, but also their descendants and text nodes.
 
   let $newArticle = $('article.template').clone();
+  $newArticle.removeClass('.template');
   /* DONE: This cloned article still has a class of template. In our modules.css stylesheet, we should give all elements with a class of template a display of none so that our template does not display in the browser. But, we also need to make sure we're not accidentally hiding our cloned article. */
 
-  if (!this.publishedOn) $newArticle.addClass('draft');
+  if (!this.publishedOn) {$newArticle.addClass('draft');}
   $newArticle.attr('data-category', this.category);
 
-  /* TODO: Now use jQuery traversal and setter methods to fill in the rest of the current template clone with values of the properties of this particular Article instance.
+  /* DONE: Now use jQuery traversal and setter methods to fill in the rest of the current template clone with values of the properties of this particular Article instance.
     We need to fill in:
       1. author name,
       2. author url,
       3. article title,
       4. article body, and
       5. publication date. */
-  $newArticle (
-    
-  )
-
+  $newArticle.find('a').text(this.author)
+  $newArticle.find('a').attr('href', this.authorUrl)
+  $newArticle.find('h1').text(this.title)
+  $newArticle.find('.article-body').html(this.body)
+  $newArticle.find('time').attr('datetime', this.publishedOn)
   // REVIEW: Display the date as a relative number of 'days ago'
   $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
   $newArticle.append('<hr>');
@@ -48,12 +50,22 @@ rawData.sort(function(a,b) {
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
 });
 
-// TODO: Refactor these for loops using the .forEach() array method.
+// DONE: Refactor these for loops using the .forEach() array method.
 
-for(let i = 0; i < rawData.length; i++) {
-  articles.push(new Article(rawData[i]));
-}
+rawData.forEach(function(createContent) {
+  articles.push(new Article(createContent));
 
-for(let i = 0; i < articles.length; i++) {
-  $('#articles').append(articles[i].toHtml());
-}
+});
+
+// for(let i = 0; i < rawData.length; i++) {
+//   articles.push(new Article(rawData[i]));
+// }
+
+articles.forEach(function(appendArticles){
+  $('#articles').append(appendArticles.toHtml());
+});
+
+
+// for(let i = 0; i < articles.length; i++) {
+//   $('#articles').append(articles[i].toHtml());
+// }
